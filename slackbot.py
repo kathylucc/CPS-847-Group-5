64753248dc1e9def80f6b945bf8cedfcccc184c1
melvinly <melvinly@gmail.com>
2020-01-31 20:20:03 -0500

    Loop added

--- slackbot.py
+++ slackbot.py
@@ -6,19 +6,20 @@ f = open('token.txt', 'r')
 token = f.read()
 f.close()
 
+#General server and bot ID
 general = 'CT5KL1G3X'
 botId = 'UTDKZ9F6U'
 
+#Connect to server
 client = slack.WebClient(token)
 
-history = client.conversations_history(channel = 'CT5KL1G3X')
+while True:
+    #Fetch history of general chat
+    history = client.conversations_history(channel = 'CT5KL1G3X')
+    messages = history.get('messages')
+    latest = messages[0]
 
-messages = history.get('messages')
-
-latest = messages[0]
-
-if latest.get('type') == 'message':
-    text = latest.get('text')
-    if text[0:5] == '!echo' and len(text) > 6:
-        print(text[0:5])
-        client.chat_postMessage(channel = '#general', text = text[5:])
+    if latest.get('type') == 'message':
+        text = latest.get('text')
+        if text[0:5] == '!echo' and len(text) > 6:
+            client.chat_postMessage(channel = '#general', text = text[5:])
