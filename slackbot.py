5a7992d3b5f1f4de362701408b44ed4faf3e3634
melvinly <melvinly@gmail.com>
2020-02-01 23:15:29 -0500

    no message

--- slackbot.py
+++ slackbot.py
@@ -1,23 +1,26 @@
 import sys
 import slack
 
+#Read file
 f = open('token.txt', 'r')
 token = f.read()
 f.close()
 
+#General server and bot ID
 general = 'CT5KL1G3X'
 botId = 'UTDKZ9F6U'
 
+#Connect to server
 client = slack.WebClient(token)
 
-history = client.conversations_history(channel = 'CT5KL1G3X')
-
-messages = history.get('messages')
-
-latest = messages[0]
-
-if latest.get('type') == 'message':
-    text = latest.get('text')
-    if text[0:5] == '!echo' and len(text) > 6:
-        print(text[0:5])
-        client.chat_postMessage(channel = '#general', text = text[5:])
+while True:
+    #Fetch history of general chat
+    history = client.conversations_history(channel = 'CT5KL1G3X')
+    messages = history.get('messages')
+    latest = messages[0]
+    #Check what type it read
+    if latest.get('type') == 'message':
+        text = latest.get('text')
+        if text[0:5] == '!echo' and len(text) > 6:
+            #Echo to chat
+            client.chat_postMessage(channel = '#general', text = text[5:])
